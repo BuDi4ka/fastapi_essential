defbaba8e622dfc7d5486685249b58d4bb276fd7
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-04-28 14:40:54 +0100

    cookie parameters

--- user_guide/main.py
+++ user_guide/main.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import Cookie, FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
-    return {"ads_id": ads_id}
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-Token values": x_token}
